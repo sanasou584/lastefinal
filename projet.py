47c22743a3af6e725785d452963f4ff1d6d56ce4
benabidasma@gmail.com <benabidsana@gmail.com>
2025-03-13 08:03:48 +0100

    fusion squash

--- projet.py
+++ projet.py
@@ -6,6 +6,15 @@ def somme (T):
 		return S
 
 Data=[1,3,5]
+
 Som=sum
 print('la somme est:',Som)
+
+
+if Data:
+	print('la somme est:',sum(Data))
+	print('la somme est:',min(Data))
+	print('la somme est:',max(Data))
+esle:
+	print('dossier vide')
 
